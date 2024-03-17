a63aa729e17d91b1da5f0d798a0bf66400b80f97
Harpan <milovansryzen@gmail.com>
2024-03-17 10:28:57 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -2,4 +2,7 @@ const add = (a, b) => a + b;
 console.log(add(1,2));
 
 const subtract = (a, b) => a - b;
-console.log(add(5, 2))
+console.log(subtract(5, 2))
+
+const multiply = (a,b) => a * b;
+console.log(multiply(2,2))
